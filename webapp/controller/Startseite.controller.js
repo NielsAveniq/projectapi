dfc23bb3bb6ad3e8cd91dd07efd9cf971db31f1c
NielsAveniq <niels.vonschack@aveniq.ch>
2022-03-17 09:20:08 +0000

    jsonmodel vor hilf

--- webapp/controller/Startseite.controller.js
+++ webapp/controller/Startseite.controller.js
@@ -1,24 +1,80 @@
 sap.ui.define([
+    'sap/ui/core/Fragment',
     "sap/ui/core/mvc/Controller", 
+    'sap/ui/model/json/JSONModel',
+    'sap/ui/model/odata/v2/ODataModel',
     'sap/m/MessageToast'
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller, MessageToast) {
+    function (Fragment, Controller, JSONModel, ODataModel, MessageToast) {
         "use strict";
 
         return Controller.extend("projectapi.controller.Startseite", {
             onInit: function () {
+                // set explored app's demo model on this sample
+			    var oModel = new JSONModel(sap.ui.require.toUrl("sap/opu/odata/sap/ERP_ISU_UMC/"));
+                oModel.attachRequestCompleted(function() {
+                    this.byId('edit').setEnabled(true);
+                }.bind(this));
+			    this.getView().setModel(oModel);
 
-                //Binding der Daten an das Smartform
-                this.getView().byId("KundeGroup").bindElement("/Accounts('713')");
-                this.getView().byId("SFTel").bindElement("/AccountAddressDependentPhones(AccountID='713',AddressID='26505',SequenceNo='001')");
-                this.getView().byId("SFMail").bindElement("/AccountAddressDependentEmails(AccountID='713',AddressID='26505',SequenceNo='001')");
-                this.getView().byId("AddresseGroup").bindElement("/AccountAddresses(AccountID='713',AddressID='26505')");
+			    this.getView().bindElement("/AccountAddresses(AccountID='713',AddressID='26505')");
 
+			    this._formFragments = {};
                 // Set the initial form to be the display one
-			this._showFormFragment("Display");
+			    this._showFormFragment("Display");
+            },
+
+            handleEditPress : function () {
+			    //this._oAddress = Object.assign({}, this.getView().getModel().getData());
+                this._toggleButtonsAndView(true);
+            },
+    
+            handleCancelPress : function () {
+                this._toggleButtonsAndView(false);
+            },
+    
+            handleSavePress : function () {
+                this._toggleButtonsAndView(false);
+            },
+    
+            _toggleButtonsAndView : function (bEdit) {
+                var oView = this.getView();
+    
+                // Show the appropriate action buttons
+                oView.byId("edit").setVisible(!bEdit);
+                oView.byId("save").setVisible(bEdit);
+                oView.byId("cancel").setVisible(bEdit);
+    
+                // Set the right form type
+                this._showFormFragment(bEdit ? "Change" : "Display");
+            },
+    
+
+            _getFormFragment: function (sFragmentName) {
+                var pFormFragment = this._formFragments[sFragmentName],
+                    oView = this.getView();
+    
+                if (!pFormFragment) {
+                    pFormFragment = Fragment.load({
+                        id: oView.getId(),
+                        name: "projectapi.view." + sFragmentName
+                    });
+                    this._formFragments[sFragmentName] = pFormFragment;
+                }
+    
+                return pFormFragment;
+            },
+
+            _showFormFragment : function (sFragmentName) {
+                var oPage = this.byId("page");
+    
+                oPage.removeAllContent();
+                this._getFormFragment(sFragmentName).then(function(oVBox){
+                    oPage.insertContent(oVBox);
+                });
             }
         });
     });
